d48ad63eaa3250176f5fc611542d739f07eb1881
bryle <brylemp@yahoo.com>
2021-01-13 20:01:56 +0800

    Added date selector

--- routes/usersPage.js
+++ routes/usersPage.js
@@ -6,16 +6,50 @@ const transactionsModel = require('../models/transactions')
 
 const router = express.Router()
 
-router.get('/', async(req,res)=>{
-    const drivers = await driverModel.find()
-    const transactions = await transactionsModel.find().sort([['date', -1]])
-    
-    res.render('./usersPage',{drivers,transactions})
-})
-
 router.get('/adddriver', (req,res)=>{
     res.render('./addDriver')
 })
 
+router.get('/sakay', (req,res)=>{
+    res.render('./tempRide.ejs')
+})
+
+router.post('/sakay', async (req,res)=>{
+    const { driverId,studentId,date } = req.body
+    const transaction = await transactionsModel.create({ driverId,studentId,date })
+    console.log(transaction)
+    res.redirect('/sakay')
+})
+
+router.get('/', async(req,res)=>{
+    const { date } = req.query
+    console.log(date)
+    let findDate
+    if(!date){
+        findDate = new Date(Date.now())
+        findDate.setHours(0, 0, 0, 0);
+    }
+    else{
+        findDate = new Date(date);
+        findDate.setHours(0, 0, 0, 0);
+    }
+    
+    const drivers = await driverModel.find()
+    const transactions = await transactionsModel.find().sort([['date', -1]])
+
+    for(let driver of drivers){
+        driver.balanceOnDate = 0
+        for(let transaction of transactions){
+            if(transaction.driverId === driver._id.toString()){
+                const dateOfT = new Date(transaction.date)
+                dateOfT.setHours(0, 0, 0, 0);
+                if(dateOfT.valueOf() === findDate.valueOf()){
+                    driver.balanceOnDate += 5
+                }
+            }
+        }
+    }
+    res.render('./usersPage',{drivers,transactions})
+})
 
 module.exports = router
